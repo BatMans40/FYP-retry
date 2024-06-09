b29ecd0d1e2506cbb1b822d64f294257c4649fdb
M. Sameel Khan <sameelkhan2010@gmail.com>
2024-06-09 19:08:10 +0500

    Revert "Merge pull request #1 from BatMans40/dev-branch"
    This reverts commit ede02043f717f404ee4473ab4d05f8fcf9335ffd, reversing
    changes made to 95edb03a13ddce366f6ce8f07d3ebf44be405218.

--- FYP/Assets/New Folder/Assets/Scripts/MenuButtonController.cs
+++ FYP/Assets/New Folder/Assets/Scripts/MenuButtonController.cs
@@ -4,52 +4,38 @@ using UnityEngine;
 
 public class MenuButtonController : MonoBehaviour {
 
-    public int index;
-    [SerializeField] bool keyDown;
-    [SerializeField] int maxIndex;
-    public AudioSource audioSource;
-    [SerializeField] MenuButton[] menuButtons;
+	// Use this for initialization
+	public int index;
+	[SerializeField] bool keyDown;
+	[SerializeField] int maxIndex;
+	public AudioSource audioSource;
 
-    void Start () {
-        audioSource = GetComponent<AudioSource>();
-        UpdateButtonSelection();
-    }
-    
-    void Update () {
-        if(Input.GetAxis ("Vertical") != 0){
-            if(!keyDown){
-                if (Input.GetAxis ("Vertical") < 0) {
-                    if(index < maxIndex){
-                        index++;
-                    } else {
-                        index = 0;
-                    }
-                } else if(Input.GetAxis ("Vertical") > 0){
-                    if(index > 0){
-                        index--; 
-                    } else {
-                        index = maxIndex;
-                    }
-                }
-                keyDown = true;
-                UpdateButtonSelection();
-            }
-        } else {
-            keyDown = false;
-        }
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis ("Vertical") != 0){
+			if(!keyDown){
+				if (Input.GetAxis ("Vertical") < 0) {
+					if(index < maxIndex){
+						index++;
+					}else{
+						index = 0;
+					}
+				} else if(Input.GetAxis ("Vertical") > 0){
+					if(index > 0){
+						index --; 
+					}else{
+						index = maxIndex;
+					}
+				}
+				keyDown = true;
+			}
+		}else{
+			keyDown = false;
+		}
+	}
 
-        if (Input.GetButtonDown("Submit")) {
-            menuButtons[index].Press();
-        }
-    }
-
-    void UpdateButtonSelection() {
-        for (int i = 0; i < menuButtons.Length; i++) {
-            if (i == index) {
-                menuButtons[i].Select();
-            } else {
-                menuButtons[i].Deselect();
-            }
-        }
-    }
 }
